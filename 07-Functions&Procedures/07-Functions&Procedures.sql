9b26fa29986cd4db7f3d06c57ded514b49f9fe57
MiroslavPK <miroslavkarakostov@gmail.com>
2023-10-14 14:40:55 +0300

    Small modification

--- 07-Functions&Procedures/07-Functions&Procedures.sql
+++ 07-Functions&Procedures/07-Functions&Procedures.sql
@@ -302,31 +302,32 @@ CREATE TABLE notification_emails(
 
 
 CREATE OR REPLACE FUNCTION 
-	trigger_fn_send_email_on_balance_change(
-) RETURNS TRIGGER
-AS
+	trigger_fn_send_email_on_balance_change() 
+RETURNS 
+	TRIGGER 
+AS 
 $$
-	BEGIN
-		INSERT INTO 
-			notification_emails(recipient_id, subject, body)
-		VALUES
-			(
-				NEW.account_id,
-				'Balance change for account: %', NEW.account_id,
-				'On % your balance was changed from % to %.', DATE(), NEW.old_sum, NEW.new_sum
-			);
-	END;
+BEGIN
+	INSERT INTO 
+		notification_emails(recipient_id, subject, body)
+	VALUES
+		(
+			NEW.account_id,
+			'Balance change for account: ' || NEW.account_id,
+			'On ' || DATE(NOW()) || ' your balance was changed from ' || NEW.old_sum || ' to ' || NEW.new_sum || '.'
+		);
+
+	RETURN NEW;
+END;
 $$
 LANGUAGE plpgsql;
 
 
-CREATE TRIGGER 
+CREATE OR REPLACE TRIGGER 
 	tr_send_email_on_balance_change
-AFTER UPDATE ON 
-	logs
+AFTER UPDATE ON logs
 FOR EACH ROW 
 	WHEN 
 		(OLD.new_sum <> NEW.new_sum)
 EXECUTE FUNCTION
 	trigger_fn_send_email_on_balance_change();
-	
